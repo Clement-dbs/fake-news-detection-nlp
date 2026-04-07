9d6c0724e02a88dbf14508c05ef1c117ad762747
clement <clement.duboisdbs@gmail.com>
2026-04-07 16:59:18 +0200

    Update `utils.py` modules to clean data

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords') # python -m spacy download en_core_web_sm
+stop_words = set(stopwords.words('english'))
+negations = {"not", "no", "never", "neither"} # On exclue ces stopwords car elle influent sur le sens de la phrase
+custom_stop_words = stop_words - negations
 
 def load_titles(filepath:str) -> pd.DataFrame:
     data = pd.read_csv(filepath)
@@ -14,7 +23,44 @@ def load_titles(filepath:str) -> pd.DataFrame:
 
     return data
 
-if __name__ == "__main__":
-    FILE_PATH = "./data/fake_or_real_news.csv"
-    data = load_titles(FILE_PATH)
-    print(data.head(5))
+contractions_dict = {
+        "don't": "do not",
+        "doesn't": "does not",
+        "didn't": "did not",
+        "can't": "cannot",
+        "couldn't": "could not",
+        "won't": "will not",
+        "wouldn't": "would not",
+        "isn't": "is not",
+        "aren't": "are not",
+        "wasn't": "was not",
+        "weren't": "were not",
+        "haven't": "have not",
+        "hasn't": "has not",
+        "hadn't": "had not",
+        "i'm": "i am",
+        "you're": "you are",
+        "they're": "they are",
+        "it's": "it is",
+        "there's": "there is",
+        "we're": "we are",
+        "she's": "she is",
+        "he's": "he is"
+    }
+
+def clean_title(text:str) -> str:
+    text = text.lower() # Mise en minuscules
+    text = re.sub(r'@\w+', '', text) # Suppression des URLs et des mentions de type @username
+    text = re.sub(r'[^\w\s]', '', text)  # Suppression de la ponctuation
+    text = re.sub(r'\b\d+\b', '', text)  # Suppression des nombres isolés
+    
+    contraction_pattern = re.compile(r'\b(' + '|'.join(re.escape(k) for k in contractions_dict.keys()) + r')\b') # Remplace les contractions
+    text = contraction_pattern.sub(lambda x: contractions_dict[x.group()], text)
+
+    tokens = word_tokenize(text)
+    tokens = [token for token in tokens if token.lower() not in custom_stop_words]
+    tokens = [WordNetLemmatizer().lemmatize(token) for token in tokens]
+    
+    return tokens
+    
+
